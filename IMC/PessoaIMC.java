a9113bf27ff6c77c8ffa713050832f0d2f774caf
IgorSAmelo <103616650+IgorSAmelo@users.noreply.github.com>
2022-11-30 21:17:15 -0300

    Imc_Atualizado1

--- IMC/PessoaIMC.java
+++ IMC/PessoaIMC.java
@@ -1,32 +1,31 @@
 package IMC;
 
-public abstract class PessoaIMC extends Pessoa{
+public abstract class PessoaIMC extends Pessoa {
 
-	public PessoaIMC(String nome, String dataNascimento, double peso, double altura) {
-		super(nome, dataNascimento);
-		this.peso = peso;
-		this.altura = altura;
-	}
-	
-	private double peso;
-	private double altura;
-	
-	public double getPeso() {
-		return peso;
-	}
-	
-	public double getAltura() {
-		return altura;
-	}
-	
-	public double calcularIMC(double peso, double altura) {
-		return peso/(altura*altura);
-	}
-	
-	public String toString() {
-		return super.toString()+", Peso: " +this.peso+", Altura: "+this.altura;
-	}
-	
-	public abstract String resultIMC();
+    public PessoaIMC(String nome, String dataNascimento, double peso, double altura) {
+        super(nome, dataNascimento);
+        this.peso = peso;
+        this.altura = altura;
+    }
 
+    private double peso;
+    private double altura;
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public double calcularIMC(double peso, double altura) {
+        return peso / (altura * altura);
+    }
+
+    public String toString() {
+        return super.toString() + ", Peso: " + this.peso + ", Altura: " + this.altura;
+    }
+
+    public abstract String resultIMC();
 }
